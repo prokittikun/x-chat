b3e3e53f54de41968eafd20bd34fbbc0ea394044
prokittikun <pro.kittikun.2546@gmail.com>
2023-02-14 02:44:22 +0700

    Refactor: messages position

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -8,7 +8,7 @@ const Message = (message: MessageInterface) => {
   return (
     <>
       <div
-        className={`flex w-full ${
+        className={`flex w-full py-1 ${
           message.uid === session?.uid ? "justify-end" : "justify-start"
         }`}
       >
@@ -18,9 +18,15 @@ const Message = (message: MessageInterface) => {
             src="https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1200,h_630/w_80,x_15,y_15,g_south_west,l_Klook_water_br_trans_yhcmh3/activities/ype8x0zkqbv239asgx9p/%E0%B8%9A%E0%B8%B1%E0%B8%95%E0%B8%A3%E0%B9%80%E0%B8%82%E0%B9%89%E0%B8%B2%E0%B8%AA%E0%B8%A7%E0%B8%99%E0%B8%AA%E0%B8%99%E0%B8%B8%E0%B8%81%E0%B9%84%E0%B8%AD%E0%B9%80%E0%B8%AD%E0%B9%87%E0%B8%A1%E0%B8%88%E0%B8%B5%20%E0%B9%80%E0%B8%A7%E0%B8%B4%E0%B8%A5%E0%B8%94%E0%B9%8C%20%E0%B8%AD%E0%B8%AD%E0%B8%9F%20%E0%B9%81%E0%B8%AD%E0%B8%94%E0%B9%80%E0%B8%A7%E0%B8%99%E0%B9%80%E0%B8%88%E0%B8%AD%E0%B8%A3%E0%B9%8C%20(IMG%20Worlds%20of%20Adventure)%20%E0%B9%83%E0%B8%99%E0%B8%94%E0%B8%B9%E0%B9%84%E0%B8%9A.jpg"
             alt="user avatar"
           /> */}
+          <p
+            className={`px-1 py-1 ${
+              message.uid === session?.uid ? "text-right " : "text-left"
+            }`}
+          >
+            {message.displayName}
+          </p>
           <div className="bg-base-content rounded-lg">
-            {/* <p className="text-right">{message.name}</p> */}
-            <p className="chat-bubble">{message.text}</p>
+            <p className="chat-bubble ">{message.text}</p>
           </div>
         </div>
       </div>

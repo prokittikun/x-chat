bec774f465d291d6b3a96110856ee5680a262638
prokittikun <pro.kittikun.2546@gmail.com>
2023-02-13 16:51:25 +0700

    Refactor: debug code

--- src/components/toast.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-export default function Toast(props: string) {
-  
-  // todo
-}
--- src/hooks/useSession.ts
+++ src/hooks/useSession.ts
@@ -1,7 +1,7 @@
 import { auth } from "./../configs/firebase";
 import { onAuthStateChanged, User } from "firebase/auth";
 import React, { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 
 export default function useSession() {
   const [userData, setUserData] = useState<User | null>(null);
@@ -14,8 +14,6 @@ export default function useSession() {
       if (user) {
         setStatus("auth");
         setUserData(user);
-        console.log(user);
-        
       } else {
         setStatus("unauth");
         setUserData(null);

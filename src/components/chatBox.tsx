f9071fe171d7c421c13eb4a4e9d1d5eb4b402005
prokittikun <pro.kittikun.2546@gmail.com>
2023-02-14 02:47:19 +0700

    Refactor: chat scroll

--- src/components/chatBox.tsx
+++ src/components/chatBox.tsx
@@ -1,22 +1,22 @@
-import { orderByChild } from "firebase/database";
 import {
   collection,
   doc,
-  getDoc,
   onSnapshot,
   orderBy,
   query,
-  where,
 } from "firebase/firestore";
 import { useEffect, useRef, useState } from "react";
 import { fireStore } from "../configs/firebase";
+import "../css/chatBox.css";
+import useSession from "../hooks/useSession";
 import { MessageInterface } from "../interfaces/message";
 import Message from "./messages";
 
 export default function ChatBox() {
   const [messages, setMessages] = useState<MessageInterface[]>([]);
-  // const scroll = useRef<HTMLDivElement>(null);
-
+  const { userData: session } = useSession();
+  let scrollIntoView = useRef<boolean>(true);
+  const scroll = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const roomCollection = collection(fireStore, "rooms");
     const roomRef = doc(roomCollection, "X-C999");
@@ -26,22 +26,36 @@ export default function ChatBox() {
       const message2: MessageInterface[] = [];
       doc.forEach((x: any) => {
         // console.log(x.data());
+        
         message2.push({ ...x.data(), id: x.id });
       });
+      console.log(scrollIntoView.current); // TODO
+      if (scrollIntoView.current) { // && session && message2[message2.length - 1].uid !== session?.uid
+        scroll.current?.scrollIntoView({ behavior: "smooth" });
+      }
+      // console.log(message2[message2.length - 1].uid, session?.uid);
+      
       setMessages(message2);
-      // scroll.current?.scrollIntoView({ behavior: "smooth" });
     });
   }, []);
 
   return (
     <>
-      <div className="flex justify-center items-center h-full max-h-70 overflow-auto">
-        <div className=" rounded-xl w-full h-full max-h-80 px-1 flex flex-col gap-5">
-          {messages?.map((message: any) => (
+      <div
+        className="flex h-screen overflow-auto chat-scroll"
+        onScroll={(e: any) => {
+          scrollIntoView.current =
+            Math.ceil(e.target.scrollTop) >=
+            e.target.scrollHeight - e.target.offsetHeight;
+          // console.log(scrollIntoView.current);
+          
+        }}
+      >
+        <div className="rounded-xl w-full px-1 flex flex-col gap-1">
+          {messages?.map((message: MessageInterface) => (
             <Message key={message.id} {...message} />
           ))}
-          {/* <div ref={scroll}></div> */} 
-          {/* TODO */}
+          <div ref={scroll}></div>
         </div>
       </div>
     </>
